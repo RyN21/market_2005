b3ac50e98989dd1dc01c8613e606d95de68280cc
Ryan Laleh <laleh21@yahoo.com>
2020-06-10 09:12:01 -0600

    Add test for vender it exists and has attributes

---
+++ vender_test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+
+class VendorTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, @vendor
+    assert_equal 'Rocky Mountain Fresh', @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+end
+
+
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 0
+#
+# pry(main)> vendor.stock(item1, 30)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 30
+#
+# pry(main)> vendor.stock(item1, 25)
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 55
+#
+# pry(main)> vendor.stock(item2, 12)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
+# ```
